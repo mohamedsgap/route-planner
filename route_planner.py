9347c9e1128110b6ace4415f7a956e728f5e1dde
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-08-31 22:27:09 +0200

    map intersections

--- route_planner.py
+++ route_planner.py
@@ -2,9 +2,24 @@
 from helpers import Map, load_map_10, load_map_40, show_map
 import math
 
+"""
 %load_ext autoreload
 %autoreload 2
+"""
 
 map_10 = load_map_10()
 show_map(map_10)
+
+map_10.intersections
+
+# this shows that intersection 0 connects to intersections 7, 6, and 5
+map_10.roads[0]
+
+# This shows the full connectivity of the map
+map_10.roads
+
+# map_40 is a bigger map than map_10
+map_40 = load_map_40()
+show_map(map_40)
+
 
